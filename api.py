b83cca49e1204b69eb15a866007137bfa6f084ab
cynthia <cynthia.monarrez@gmail.com>
2019-09-19 19:26:46 -0500

    create basic api request

---
+++ api.py
@@ -0,0 +1,12 @@
+#Call the Chicago Food Inspection API
+response = requests.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json")
+# Decode the raw JSON data.
+response_data = response.json()
+
+print(response_data)
+
+
+#or in 1 line
+#import requests
+#response_data = requests.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json").json()
+#print(response_data)
